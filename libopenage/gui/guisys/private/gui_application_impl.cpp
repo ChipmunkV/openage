2968c2438eb599204a5d12e2c94598a058ad730e
velorums <velorums@gmail.com>
2016-01-09 13:25:22 +0100

    guisys: application

---
+++ libopenage/gui/guisys/private/gui_application_impl.cpp
@@ -0,0 +1,50 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#include "gui_application_impl.h"
+
+#include <cassert>
+
+namespace openage {
+namespace gui {
+
+std::weak_ptr<GuiApplicationImpl> GuiApplicationImpl::instance;
+
+std::shared_ptr<GuiApplicationImpl> GuiApplicationImpl::get() {
+	std::shared_ptr<GuiApplicationImpl> candidate = GuiApplicationImpl::instance.lock();
+
+	assert(!candidate || std::this_thread::get_id() == candidate->owner);
+
+	return candidate ? candidate : std::shared_ptr<GuiApplicationImpl>{new GuiApplicationImpl};
+}
+
+GuiApplicationImpl::~GuiApplicationImpl() {
+	assert(std::this_thread::get_id() == this->owner);
+}
+
+void GuiApplicationImpl::processEvents() {
+	assert(std::this_thread::get_id() == this->owner);
+	this->app.processEvents();
+}
+
+namespace {
+	int argc = 1;
+	char arg[] = "openage";
+	char *argv = &arg[0];
+}
+
+GuiApplicationImpl::GuiApplicationImpl()
+	:
+#ifndef NDEBUG
+	owner{std::this_thread::get_id()},
+#endif
+	app{argc, &argv}
+{
+#ifdef Q_OS_UNIX
+		// TODO: the game should set a correct locale from the start.
+		auto lc_all = qgetenv("LC_ALL");
+		if (lc_all != "C" && lc_all != "POSIX")
+			qWarning("The GUI will apply your non-C locale. But that can break the game logic. Use LC_ALL=C as workaround.");
+#endif
+}
+
+}} // namespace openage::gui
