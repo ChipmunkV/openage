4205c411e545305d86efaaf7485ef2d2c0d0b436
velorums <velorums@gmail.com>
2016-01-18 15:54:21 +0100

    guisys: image provider interface

---
+++ libopenage/gui/guisys/public/gui_image_provider.h
@@ -0,0 +1,23 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <memory>
+#include <functional>
+
+namespace openage {
+namespace gui {
+
+class GuiImageProviderImpl;
+
+class GuiImageProvider {
+public:
+	explicit GuiImageProvider(std::unique_ptr<GuiImageProviderImpl> impl);
+	~GuiImageProvider();
+
+private:
+	friend class GuiImageProviderImpl;
+	std::unique_ptr<GuiImageProviderImpl, std::function<void(GuiImageProviderImpl*)>> impl;
+};
+
+}} // namespace openage::gui
