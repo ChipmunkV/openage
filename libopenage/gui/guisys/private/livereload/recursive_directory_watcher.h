99c32b73b24fca283d94e29999bfcc09e83ecba5
velorums <velorums@gmail.com>
2016-02-26 01:59:21 +0100

    guisys: directory watcher

---
+++ libopenage/gui/guisys/private/livereload/recursive_directory_watcher.h
@@ -0,0 +1,32 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <future>
+
+#include <QObject>
+#include <QStringList>
+
+namespace openage {
+namespace gui {
+
+/**
+ * Emits a signal when anything changes in the directories.
+ */
+class RecursiveDirectoryWatcher : public QObject {
+Q_OBJECT
+
+public:
+	explicit RecursiveDirectoryWatcher(QObject *parent = nullptr);
+	virtual ~RecursiveDirectoryWatcher();
+
+signals:
+	void changeDetected();
+	void rootDirsPathsChanged(const QStringList&);
+	void quit();
+
+private:
+	std::future<void> worker;
+};
+
+}} // namespace openage::gui
