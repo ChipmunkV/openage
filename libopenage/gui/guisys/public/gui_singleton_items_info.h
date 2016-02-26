98ff6438856cc71496b48d44cac64e483a12bf08
velorums <velorums@gmail.com>
2016-02-26 01:59:21 +0100

    guisys: base for info to pass to qml singletons

---
+++ libopenage/gui/guisys/public/gui_singleton_items_info.h
@@ -0,0 +1,14 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+namespace openage {
+namespace gui {
+
+/**
+ * Accessible during creation of any singleton QML item.
+ */
+struct GuiSingletonItemsInfo {
+};
+
+}} // namespace openage::gui
