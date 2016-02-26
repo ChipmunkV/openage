66fea3a3f95b9af0a771678d084596168d6e5fda
velorums <velorums@gmail.com>
2016-02-26 01:59:21 +0100

    guisys: thread synchronization wrapper

---
+++ libopenage/gui/guisys/public/gui_event_queue.cpp
@@ -0,0 +1,22 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#include "../public/gui_event_queue.h"
+
+#include "../private/gui_event_queue_impl.h"
+
+namespace openage {
+namespace gui {
+
+GuiEventQueue::GuiEventQueue()
+	:
+	impl{std::make_unique<GuiEventQueueImpl>()} {
+}
+
+GuiEventQueue::~GuiEventQueue() {
+}
+
+void GuiEventQueue::process_callbacks() {
+	this->impl->process_callbacks();
+}
+
+}} // namespace openage::gui
