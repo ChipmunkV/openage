54745040cd805d297f50a54e967bf4bc167220e5
velorums <velorums@gmail.com>
2016-02-26 01:59:21 +0100

    guisys: custom render control that adopts a window

---
+++ libopenage/gui/guisys/private/gui_render_control.cpp
@@ -0,0 +1,21 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#include "gui_render_control.h"
+
+namespace openage {
+namespace gui {
+
+RenderControl::RenderControl(QWindow *w)
+	:
+	QQuickRenderControl{}, //
+	window{w} {
+}
+
+QWindow* RenderControl::renderWindow(QPoint *offset) {
+	if (offset)
+		*offset = QPoint{0, 0};
+
+	return this->window;
+}
+
+}} // namespace openage::gui
